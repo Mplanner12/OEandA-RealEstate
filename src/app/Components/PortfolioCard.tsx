61d318b5b89b43899e1b651d57d9f0c673cd4d53
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-10-02 11:39:57 +0100

    porfolio

--- src/app/Components/PortfolioCard.tsx
+++ src/app/Components/PortfolioCard.tsx
@@ -13,20 +13,20 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({
   images,
 }) => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-start w-full md:justify-between py-6 mb-[2rem] md:mb-[3.5rem]">
+    <div className="flex flex-col md:flex-row items-center justify-start w-full md:justify-between py-6 mb-[3rem] md:mb-[3.5rem] gap-y-[3rem]">
       {/* Left side: Text and button */}
       <div className="w-full md:w-1/2 flex flex-col justify-start items-start">
-        <h2 className="text-[3rem] uppercase font-bold w-[85%] text-red-950">
+        <h2 className="text-[2.25rem] md:text-[3rem] uppercase font-bold w-[85%] text-red-950">
           {title}
         </h2>
         <p className="text-gray-700 mt-4 w-[80%]">{description}</p>
-        <button className="mt-6 py-2 px-4 bg-red-600 text-white font-normal rounded-md">
+        <button className="mt-9 md:mt-6 py-2 px-4 bg-red-600 text-white font-normal rounded-md">
           Explore Properties
         </button>
       </div>
 
       {/* Right side: Images */}
-      <div className="w-full justify-end items-center md:w-1/2 flex flex-wrap">
+      <div className="w-full justify-start lg:justify-end items-center md:w-1/2 flex flex-wrap">
         {images.map((img, index) => (
           <img
             key={index}
